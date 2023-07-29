8a397f3e66ff835bdfcae36c57ae197030237fd1
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-07-29 23:44:57 +0630

    29 Jul 11:44 PM

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,8 @@
  class HelloWorld{
 
+    // Java Recursion
+    //
+
     //    int[][] myNumbers = {{ 1,2,3,4 },{5,6,7}};
     //    for(int i=0;i<myNumbers.length;i++)
     //    {
@@ -18,16 +21,36 @@
     //     return 5 + x;
     // }
 
-    static void checkAge(int age)
-    {
+    // static void checkAge(int age)
+    // {
+    //     if(age < 18){
+    //         System.out.println("Access denied - You are not old enough");
+    //     }else{
+    //         System.out.println("Access granted - You are old enough!");
+    //     }
+    // }
 
-    }
-    
+    // static int plusMethodInt(int x,int y){
+    //     return x + y;
+    // }
+
+    // static double plusMethodDouble(double x, double y)
+    // {
+    //     return x + y;
+    // }
+
     public static void main(String args[]){
 
 
-        System.out.println(myMethod(3));
 
+        // int myNum1 = plusMethodInt(8,5);
+        // double myNum2 = plusMethodDouble(4.3,6.26);
+
+        // System.out.println("int: " + myNum1);
+        // System.out.println("double: " + myNum2);
+
+        // checkAge(20);
+        // System.out.println(myMethod(3));
         // myMethod("Liam");
         // myMethod("Jenny");
         // myMethod("Anja");
