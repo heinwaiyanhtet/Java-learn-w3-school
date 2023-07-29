fa1b1a9519fe5d3f6dacbd645c8153abb9f360e7
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-07-30 01:49:07 +0630

    Before OOP learning

--- HelloWord.java
+++ HelloWord.java
@@ -38,10 +38,19 @@
     // {
     //     return x + y;
     // }
+  
 
     public static void main(String args[]){
 
-        
+
+            
+        // int result =  sum(10);
+        // System.out.println(result);
+        // int result = sum(10);
+        // System.out.println(result);
+
+        // int result = sum(5,10);
+        // System.out.println(result);
 
         // int myNum1 = plusMethodInt(8,5);
         // double myNum2 = plusMethodDouble(4.3,6.26);
@@ -56,5 +65,40 @@
         // myMethod("Anja");
     
     }
+
+    // public static int sum(int start,int end)
+    // {
+    //     if(end > start)
+    //     {
+    //         return end + sum(start,end - 1);
+    //     }
+    //     else
+    //     {
+    //         return end;
+    //     }
+        
+    // }
+
+    // public static int sum(int k){
+    //     if(k > 0)
+    //     {
+    //         return k + sum(k - 1);
+    //     }
+    //     else
+    //     {
+    //         return 0;
+    //     }
+    // }
+
+    // public static int sum(int k){
+    //     if(k > 0)
+    //     {
+    //         return k + sum(k-1);
+    //     }
+    //     else
+    //     {
+    //         return 0;
+    //     }
+    // }
 
 }   
