254b91af20f1109be7eed1711032502c130db172
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-07-29 23:17:30 +0630

    29 Jul 11:17 PM

---
+++ HelloWord.java
@@ -0,0 +1,37 @@
+ class HelloWorld{
+
+    //    int[][] myNumbers = {{ 1,2,3,4 },{5,6,7}};
+    //    for(int i=0;i<myNumbers.length;i++)
+    //    {
+    //         for(int j=0;j<myNumbers[i].length;j++)
+    //         {
+    //             System.out.println(myNumbers[i][j]);
+    //         }
+    //    }
+
+    // static void myMethod(String fname)
+    // {
+    //     System.out.println(fname + " Refsnes");
+    // }
+
+    // static int myMethod(int x){
+    //     return 5 + x;
+    // }
+
+    static void checkAge(int age)
+    {
+
+    }
+    
+    public static void main(String args[]){
+
+
+        System.out.println(myMethod(3));
+
+        // myMethod("Liam");
+        // myMethod("Jenny");
+        // myMethod("Anja");
+    
+    }
+
+}   
